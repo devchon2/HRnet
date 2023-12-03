3ba64a17cc06527867712d1324816a526eaf3f9e
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-12-03 23:56:46 +0100

    Add show state to CreateUserPage component

--- src/Pages/Components/Modale/Modale.jsx
+++ src/Pages/Components/Modale/Modale.jsx
@@ -3,138 +3,212 @@ import styled from "styled-components";
 import { useState } from "react";
 
 const StyledBGContainer = styled.div`
-  display: ${(props) => props.BgContainerDisplay};
-  z-index: ${(props) => props.BgContainerZIndex};
-  width: ${(props) => props.BgContainerWidth};
-  height: ${(props) => props.BgContainerHeight};
-  background-color: ${(props) => props.BgContainerColor};
-  opacity: ${(props) => props.BgContainerOpacity};
-  position: ${(props) => props.BgContainerPos};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.BgColor};
+  opacity: ${(props) => props.Opacity};
+  position: absolute;
+  border-radius : ${(props) => props.BorderRadius};
 `;
 
 const StyledModaleContainer = styled.div`
-  display: ${(props) => props.ModaleContainerDisplay};
-  width: ${(props) => props.ModaleContainerWidth};
-  height: ${(props) => props.ModaleContainerHeight};
-  background-color: ${(props) => props.ModaleContainerColor};
-  opacity: ${(props) => props.ModaleContainerOpacity};
-  position: ${(props) => props.ModaleContainerPos};
+  display: flex;
+  width: ${(props) =>
+    props.ModaleSize === "small"
+      ? "30vw"
+      : props.ModaleSize === "medium"
+        ? "50vw"
+        : props.ModaleSize === "large"
+          ? "70vw"
+          : props.ModaleSize === "full"
+            ? "100vw"
+            : props.ModaleContainerWidth};
+  height: ${(props) =>
+    props.ModaleSize === "small"
+      ? "30vh"
+      : props.ModaleSize === "medium"
+        ? "50vh"
+        : props.ModaleSize === "large"
+          ? "70vh"
+          : props.ModaleSize === "full"
+            ? "100vh"
+            : props.ModaleContainerHeight};
+  background-color: ${(props) => props.ModaleBoxColor};
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  opacity: 1;
+  z-index: 2;
 `;
 
 const StyledModaleContent = styled.div`
-  display: ${(props) => props.ModaleContentDisplay};
-  width: ${(props) => props.ModaleContentWidth};
-  height: ${(props) => props.ModaleContentHeight};
-  background-color: ${(props) => props.ModaleContentColor};
-  opacity: ${(props) => props.ModaleContentOpacity};
-  position: ${(props) => props.ModaleContentPos};
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.ModaleColor};
 `;
 
 const StyledModaleCloseButton = styled.button`
-  display: ${(props) => props.ModaleCloseButtonDisplay};
-  width: ${(props) => props.ModaleCloseButtonWidth};
-  height: ${(props) => props.ModaleCloseButtonHeight};
-  background-color: ${(props) => props.ModaleCloseButtonColor};
-  opacity: ${(props) => props.ModaleCloseButtonOpacity};
-  position: ${(props) => props.ModaleCloseButtonPos};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding:5px;
+  width: 30px;
+  height: 30px;
+  background-color: ${(props) => props.ButtonColor};
+  position: absolute;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  top:-10px;
+  right:-10px;
+
+  &:hover {
+    scale: 1.1;
+  }
+
+
 `;
 
-const StyledModaleCloseButtonContent = styled.div`
-  display: ${(props) => props.ModaleCloseButtonContentDisplay};
-  z-index: ${(props) => props.ModaleCloseButtonContentZIndex};
-  width: ${(props) => props.ModaleCloseButtonContentWidth};
-  height: ${(props) => props.ModaleCloseButtonContentHeight};
-  background-color: ${(props) => props.ModaleCloseButtonContentColor};
-  opacity: ${(props) => props.ModaleCloseButtonContentOpacity};
-  position: ${(props) => props.ModaleCloseButtonContentPos};
+const StyledModaleCloseButtonContent = styled.p`
+  display: flex;
+  
+  
+  
+  
+  color: ${(props) => props.itemButtonColor};
+  font-size: ${(props) => props.itemButtonSize};
+
 `;
 
-const StyledModaleMessage = styled.p`
-  display: ${(props) => props.ModaleMessageDisplay};
-  z-index: ${(props) => props.ModaleMessageZIndex};
-  width: ${(props) => props.ModaleMessageWidth};
-  height: ${(props) => props.ModaleMessageHeight};
-  background-color: ${(props) => props.ModaleMessageColor};
-  opacity: ${(props) => props.ModaleMessageOpacity};
-  position: ${(props) => props.ModaleMessagePos};
+const StyledModaleMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  color: ${(props) => props.ModaleMessageColor};
+  font-size: ${(props) => props.ModaleMessageSize};
+
 `;
 
+export default function Modale({
+  Show = false,
+  BgColor = "black",
+  Opacity = "0.5",
+
+  ModaleSize = "medium",
+  ModaleColor = "white",
+
+
+  ButtonColor = "black",
+  ItemButtonColor = "white",
+  ItemButtonSize = "20px",
+  TextColor = "black",
+  TextSize = "20px",
+  Message = "Employee Created",
+}) {
+  const [show, setShow] = useState(Show);
+  const [modaleSize, setModaleSize] = useState(ModaleSize);
+  const [modaleColor, setModaleColor] = useState(ModaleColor);
+  const [bgColor, setBgColor] = useState(BgColor);
+  const [opacity, setOpacity] = useState(Opacity);
+  const [buttonColor, setButtonColor] = useState(ButtonColor);
+  const [itemButtonColor, setItemButtonColor] = useState(ItemButtonColor);
+  const [itemButtonSize, setItemButtonSize] = useState(ItemButtonSize);
+  const [textColor, setTextColor] = useState(TextColor);
+  const [textSize, setTextSize] = useState(TextSize);
+  const [message, setMessage] = useState(Message);
 
 
 
-export default function Modale(
-    {
-        BgContainer,
-        ModaleContainer,
-        ModaleMessage,
-        
-        ContentWidth,
-        ContentHeight,
-        ContentPos,
-        ContentZIndex,
-        ContentColor,
-        ContentOpacity,
-        
-        CloseButtonWidth,
-        CloseButtonHeight,
-        CloseButtonPos,
-        CloseButtonZIndex,
-        CloseButtonColor,
-        CloseButtonOpacity,
-        
-        CloseButtonContentWidth,
-        CloseButtonContentHeight,
-        CloseButtonContentPos,
-        CloseButtonContentZIndex,
-        CloseButtonContentColor,
-        CloseButtonContentOpacity,
-        MessageText,
-        
-        ModaleMessageWidth,
-        ModaleMessageHeight,
-        ModaleMessagePos,
-        ModaleMessageZIndex,
-        ModaleMessageColor,
-        ModaleMessageOpacity
-    }
+  useEffect(() => {
+    setShow(Show);
+    setModaleSize(ModaleSize);
+    setModaleColor(ModaleColor);
+    setBgColor(BgColor);
+    setOpacity(Opacity);
+    setButtonColor(ButtonColor);
+    setItemButtonColor(ItemButtonColor);
+    setItemButtonSize(ItemButtonSize);
+    setTextColor(TextColor);
+    setTextSize(TextSize);
+    setMessage(Message);
 
-) {
+  }, [Show, ModaleSize, ModaleColor, BgColor, Opacity, ButtonColor, ItemButtonColor, ItemButtonSize, TextColor, TextSize, Message]);
 
-    const [ModaleDisplay, setModaleDisplay] = useState("none");
-    const [ModaleContainerDisplay, setModaleContainerDisplay] = useState("none");
-    const [ModaleMessageDisplay, setModaleMessageDisplay] = useState("none");
-    const [ContentDisplay, setContentDisplay] = useState("none");
-    const [CloseButtonDisplay, setCloseButtonDisplay] = useState("none");
-    const [CloseButtonContentDisplay, setCloseButtonContentDisplay] = useState("none");
+  function handleClose(e) {
 
-    useEffect(() => {
-        setModaleDisplay("block");
-        setModaleContainerDisplay("flex");
-        setModaleMessageDisplay("flex");
-        setContentDisplay("flex");
-        setCloseButtonDisplay("flex");
-        setCloseButtonContentDisplay("flex");
-    }, []);
+
+    e.preventDefault();
+    console.log("close");
+    setShow(false);
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach(input => input.value = "");
+
+
+  }
+
+  if (show) {
+
+    return (
+      <>
+        <StyledBGContainer
+          BgColor={bgColor}
+          Opacity={opacity}
 
 
 
 
+        />
+
+        <StyledModaleContainer
+          ModaleSize={modaleSize}
 
 
-    
-  return (
-   <StyledBGContainer BgContainerDisplay={BgContainer.BgContainerDisplay} BgContainerWidth={BgContainer.BgContainerWidth} BgContainerHeight={BgContainer.BgContainerHeight} BgContainerPos={BgContainer.BgContainerPos} BgContainerZIndex={BgContainer.BgContainerZIndex} BgContainerColor={BgContainer.BgContainerColor} BgContainerOpacity={BgContainer.BgContainerOpacity}>
-        <StyledModaleContainer ModaleContainerDisplay={ModaleContainer.ModaleContainerDisplay} ModaleContainerWidth={ModaleContainer.ModaleContainerWidth} ModaleContainerHeight={ModaleContainer.ModaleContainerHeight} ModaleContainerPos={ModaleContainer.ModaleContainerPos} ModaleContainerZIndex={ModaleContainer.ModaleContainerZIndex} ModaleContainerColor={ModaleContainer.ModaleContainerColor} ModaleContainerOpacity={ModaleContainer.ModaleContainerOpacity}>
-            <StyledModaleContent ModaleContentDisplay={ContentDisplay} ModaleContentWidth={ContentWidth} ModaleContentHeight={ContentHeight} ModaleContentPos={ContentPos} ModaleContentZIndex={ContentZIndex} ModaleContentColor={ContentColor} ModaleContentOpacity={ContentOpacity}>
-                <StyledModaleCloseButton ModaleCloseButtonDisplay={CloseButtonDisplay} ModaleCloseButtonWidth={CloseButtonWidth} ModaleCloseButtonHeight={CloseButtonHeight} ModaleCloseButtonPos={CloseButtonPos} ModaleCloseButtonZIndex={CloseButtonZIndex} ModaleCloseButtonColor={CloseButtonColor} ModaleCloseButtonOpacity={CloseButtonOpacity}>
-                    <StyledModaleCloseButtonContent ModaleCloseButtonContentDisplay={CloseButtonContentDisplay} ModaleCloseButtonContentWidth={CloseButtonContentWidth} ModaleCloseButtonContentHeight={CloseButtonContentHeight} ModaleCloseButtonContentPos={CloseButtonContentPos} ModaleCloseButtonContentZIndex={CloseButtonContentZIndex} ModaleCloseButtonContentColor={CloseButtonContentColor} ModaleCloseButtonContentOpacity={CloseButtonContentOpacity}>
-                        <StyledModaleMessage ModaleMessageDisplay={ModaleMessage.ModaleMessageDisplay} ModaleMessageWidth={ModaleMessage.ModaleMessageWidth} ModaleMessageHeight={ModaleMessage.ModaleMessageHeight} ModaleMessagePos={ModaleMessage.ModaleMessagePos} ModaleMessageZIndex={ModaleMessage.ModaleMessageZIndex} ModaleMessageColor={ModaleMessage.ModaleMessageColor} ModaleMessageOpacity={ModaleMessage.ModaleMessageOpacity}>
-                            {MessageText}
-                        </StyledModaleMessage>
-                    </StyledModaleCloseButtonContent>
-                </StyledModaleCloseButton>
-            </StyledModaleContent>
+        >
+          <StyledModaleContent
+            ModaleColor={modaleColor}
+
+          >
+            <StyledModaleCloseButton
+              ButtonColor={buttonColor}
+              onClick={handleClose}
+
+            >
+
+              <StyledModaleCloseButtonContent
+
+                ItemButtonColor={itemButtonColor} ItemButtonSize={itemButtonSize}
+
+
+              >{/* un code ascii qui represente une croix*/}
+                &#10006;
+
+              </StyledModaleCloseButtonContent>
+
+
+
+            </StyledModaleCloseButton>
+            <StyledModaleMessage
+              ModaleMessageColor={textColor}
+              ModaleMessageSize={textSize}
+
+
+            >
+              {message}
+            </StyledModaleMessage>
+          </StyledModaleContent>
         </StyledModaleContainer>
-    </StyledBGContainer>
-)
+      </>
+    );
+  }
+
+  return null;
 }
--- src/Pages/CreateUserPage/CreateUserPage.jsx
+++ src/Pages/CreateUserPage/CreateUserPage.jsx
@@ -24,6 +24,7 @@ export default function CreateUserPage() {
   const [onBoarding, setOnboarding] = useState("");
   const [selectedStateLocation, setSelectedStateLocation] = useState("");
   const [selectedDepartment, setSelectedDepartment] = useState("");
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -50,76 +51,77 @@ export default function CreateUserPage() {
     selectedDepartment,
   ]);
 
-  // function checkForm() {
+  function checkForm() {
 
-  //   console.log("stateLocation, department", stateLocation, department)
-  //   const infosDatas = [firstName, lastName, birthDate];
-  //   const contactDatas = [street, city, stateLocation, zip];
-  //   const onBoardingDatas = [startDate, department];
-  //   const checkInfos = infosDatas.every(element => element !== "");
-  //   const checkContact = contactDatas.every(element => element !== "");
-  //   const checkOnBoarding = onBoardingDatas.every(element => element !== "");
-  //   let validate = true;
+    console.log("stateLocation, department", stateLocation, department)
+    const infosDatas = [firstName, lastName, birthDate];
+    const contactDatas = [street, city, stateLocation, zip];
+    const onBoardingDatas = [startDate, department];
+    const checkInfos = infosDatas.every(element => element !== "");
+    const checkContact = contactDatas.every(element => element !== "");
+    const checkOnBoarding = onBoardingDatas.every(element => element !== "");
+    let validate = true;
 
-  //   console.log("checkForm", checkInfos, checkContact, checkOnBoarding)
+    console.log("checkForm", checkInfos, checkContact, checkOnBoarding)
 
-  //   if (checkInfos) {
+    if (checkInfos) {
 
-  //     setInfos({ 'firstName': firstName, 'lastName': lastName, 'birthDate': birthDate });
-  //     console.log("checkInfos OK", { 'firstName': firstName, 'lastName': lastName, 'birthDate': birthDate });
-  //   } else {
+      setInfos({ 'firstName': firstName, 'lastName': lastName, 'birthDate': birthDate });
+      console.log("checkInfos OK", { 'firstName': firstName, 'lastName': lastName, 'birthDate': birthDate });
+    } else {
 
-  //     validate = false;
-  //     console.log("checkInfos ko", { firstName: firstName, lastName: lastName, birthDate: birthDate })
-  //   }
+      validate = false;
+      console.log("checkInfos ko", { firstName: firstName, lastName: lastName, birthDate: birthDate })
+    }
 
-  //   if (checkContact) {
+    if (checkContact) {
 
-  //     console.log("checkContact", contact);
-  //     setContact({ 'street': street, 'city': city, 'state': stateLocation, 'zip': zip });
-  //     console.log("checkContact OK", { 'street': street, 'city': city, 'state': stateLocation, 'zip': zip });
-  //   } else {
+      console.log("checkContact", contact);
+      setContact({ 'street': street, 'city': city, 'state': stateLocation, 'zip': zip });
+      console.log("checkContact OK", { 'street': street, 'city': city, 'state': stateLocation, 'zip': zip });
+    } else {
 
-  //     console.log("checkContact ko", { 'street': street, 'city': city, 'state': stateLocation, 'zip': zip })
-  //     validate = false;
-  //   }
+      console.log("checkContact ko", { 'street': street, 'city': city, 'state': stateLocation, 'zip': zip })
+      validate = false;
+    }
 
-  //   if (checkOnBoarding) {
+    if (checkOnBoarding) {
 
-  //     setOnboarding({ 'startDate': startDate, 'department': department });
-  //     console.log("checkOnBoarding OK", { 'startDate': startDate, 'department': department });
-  //   } else {
+      setOnboarding({ 'startDate': startDate, 'department': department });
+      console.log("checkOnBoarding OK", { 'startDate': startDate, 'department': department });
+    } else {
 
-  //     console.log("checkOnBoarding ko", { 'startDate': startDate, 'department': department })
-  //     validate = false;
-  //   }
+      console.log("checkOnBoarding ko", { 'startDate': startDate, 'department': department })
+      validate = false;
+    }
 
-  //   return validate;
+    return validate;
 
-  // }
+  }
 
-  // function Handle_Submit(e) {
-  //   e.preventDefault();
+  function Handle_Submit(e) {
+    e.preventDefault();
 
-  //   if (checkForm()) {
-  //     console.log(firstName, lastName, birthDate, street, city, stateLocation, zip, startDate, department)
-  //     dispatch(add_infos({ 'firstName': infos.firstName, 'lastName': infos.lastName, 'birthDate': infos.birthDate }));
-  //     console.log("dispatch infos", infos);
-  //     dispatch(add_contact({ 'street': contact.street, 'city': contact.city, 'state': contact.state, 'zip': contact.zip }));
-  //     console.log("dispatch contact", contact);
-  //     dispatch(add_onboarding({ 'startDate': onBoarding.startDate, 'department': onBoarding.department }));
-  //     console.log("dispatch onBoarding", onBoarding);
+    if (checkForm()) {
+      console.log(firstName, lastName, birthDate, street, city, stateLocation, zip, startDate, department)
+      dispatch(add_infos({ 'firstName': infos.firstName, 'lastName': infos.lastName, 'birthDate': infos.birthDate }));
+      console.log("dispatch infos", infos);
+      dispatch(add_contact({ 'street': contact.street, 'city': contact.city, 'state': contact.state, 'zip': contact.zip }));
+      console.log("dispatch contact", contact);
+      dispatch(add_onboarding({ 'startDate': onBoarding.startDate, 'department': onBoarding.department }));
+      console.log("dispatch onBoarding", onBoarding);
 
-  //     //Fonction de vérification des éléments de formulaire a implémenter
+      //Fonction de vérification des éléments de formulaire a implémenter
 
-  //     dispatch(add_employee({ 'infos': infos, 'contact': contact, 'onBoarding': onBoarding }));
-
-  //     console.log("submit");
-  //   }
-  // }
+      dispatch(add_employee({ 'infos': infos, 'contact': contact, 'onBoarding': onBoarding }));
+      setShow(true);
+      console.log("submit");
+    }
+  }
   return (
+    <>
     <main>
-      {/* <h1 className={style.page_Title}>Create Employees</h1>
+      <h1 className={style.page_Title}>Create Employees</h1>
 
       <form onSubmit={Handle_Submit} className={style.form}>
         <div className={style.container_infos}>
@@ -243,54 +245,16 @@ export default function CreateUserPage() {
         <button className={style.submit_button} type="submit">
           save
         </button>
-      </form> */}
-      <Modale
-        MessageText="test"
-
-
-        //Background_Container
-        bgContainerPos="fixed"
-        bgContainerZIndex="1"
-        bgContainerWidth="100%"
-        bgContainerHeight="100%"
-        bgContainerColor="rgba(25,250,0,0.4)"
-        bgContainerOpacity="1"
-
-        //Modale_Container
-        ModaleContainerDisplay="flex"
-        ModaleContainerPos="fixed"
-        ModaleContainerZIndex="1"
-        ModaleContainerWidth="100%"
-        ModaleContainerHeight="100%"
-        ModaleContainerColor="rgba(0,0,0,0.4)"
-        ModaleContainerOpacity="1"
-
-        //Modale_Content
-        ModaleContentDisplay="flex"
-        ModaleContentPos="fixed"
-        ModaleContentZIndex="1"
-        ModaleContentWidth="100%"
-        ModaleContentHeight="100%"
-        ModaleContentColor="rgba(0,0,0,0.4)"
-        ModaleContentOpacity="1"
-
-        //Modale_Message
-        ModaleCloseButtonDisplay="flex"
-        ModaleCloseButtonPos="fixed"
-        ModaleCloseButtonZIndex="1"
-        ModaleCloseButtonWidth="100%"
-        ModaleCloseButtonHeight="100%"
-        ModaleCloseButtonColor="rgba(0,0,0,0.4)"
-        ModaleCloseButtonOpacity="1"
-
-        //Modale_CloseButtonContent
-        ModaleCloseButtonContentDisplay="flex"
-        ModaleCloseButtonContentZIndex="1"
-        ModaleCloseButtonContentWidth="100%"
-        ModaleCloseButtonContentHeight="100%"
-        ModaleCloseButtonContentColor="rgba(250,0,0,0.4)"
-        ModaleCloseButtonContentOpacity="1"
-      />
+      </form>
+      
     </main>
+    <Modale
+        
+        Show = {show}
+
+
+        
+      />
+      </>
   );
 }
