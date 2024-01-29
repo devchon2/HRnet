b0d74af5813dba551ac37271ef122a2cb4f80c46
Rachid Chon <rachidchon.adm@hotmail.fr>
2024-01-29 22:42:54 +0100

    Update Table Theme

--- src/Pages/Components/Table/Table.jsx
+++ src/Pages/Components/Table/Table.jsx
@@ -7,202 +7,244 @@ import style from "./Table.module.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // Custom theme for Material UI components, focusing on Table aesthetics.
+
 const theme = createTheme({
   components: {
     MuiPaper: {
-      // Customizes the outermost paper layout of the table
+      // Paper component for the table container.
       styleOverrides: {
         root: {
-          border: "1px solid #001c30", // Dark border for contrast
-          boxShadow: "20px 20px 20px -20px white", // Shadow for depth
-          borderRadius: "15px", // Rounded corners for modern look
-          width: "100%", // Full width
-          height: "fit-content", // Height adjusts to content
-          margin: "0 5%", // Centering with margin
+          // Overrides the default styles for the root element.
+          display:'flex',
+          flexDirection: "column",
+          margin: "2vw 2%",
+          borderRadius: "20px",
+          height: "fit-content",
+          width: "95%",
+          maxHeight: "90%",
+          backgroundColor: "#001C30 !important",
+          "& > .MuiBox-root": {
+            height: "5%",
+            width: "100%",
+            borderRadius: 0,
+            backgroundColor: "#001C30",
+            "& > .MuiBox-root": {
+              backgroundColor: "#001C30",
+              height: "100%",
+              '& > .MuiBox-root':{
+                width:'100%',
+                '& > .MuiTablePagination-root':{
+width:'100%',              }
+
+            },
+          },
         },
       },
-    },
-    MuiTableHead: {
-      // Styles for the table header
-      styleOverrides: {
-        root: {
-          height: "3vh", // Fixed height for uniformity
-          backgroundColor: "transparent", // Dark background for header
-        },
       },
     },
-    MuiTableRow: {
-      // Styles applied to table rows
+    MuiTableContainer: {
+      // Table container component.
       styleOverrides: {
         root: {
-          display: "flex",
-          justifyContent: "space-between",
+flexGrow: 1,
+          flexShrink: 1,
           maxWidth: "100%",
-          backgroundColor: "transparent", // Clear background for blend
-        },
-        head: {
-          // Special styling for header row
-          backgroundColor: "#001c30", // Matches header background color
-        },
-      },
-    },
-    MuiMenuList: {
-      // Styles for the menu list
-      styleOverrides: {
-        root: {
-          backgroundColor: "transparent", // Clear background for blend
-        },
-      },
-    },
-    MuiTableCell: {
-      // Cell-specific styling
-      styleOverrides: {
-        root: {
-          // Applies to all cells
-          fontSize: "0.6vw", // Responsive font size
-          fontWeight: "normal", // Regular weight for readability
-          justifyContent: "center", // Centering for aesthetics
-          alignItems: "center", // Centering for aesthetics
-          textAlign: "center", // Centering for aesthetics
-        },
-        head: {
-          // Header cells specifically
-          display: "flex", // Centering for aesthetics
-          fontWeight: "bold", // Bold for emphasis
-          fontSize: "0.7vw", // Slightly larger for importance
-          paddingLeft: "0", // Spacing for aesthetics
-          justifyContent: "space-between", // Centering for aesthetics
-          backgroundColor: "#001c30 !important", // Clear background for blend
-          color: "white", // White text for contrast
-          ".Mui-TableHeadCell-Content":{
-
-            display: "flex",
-            justifyContent: "space-between",
-            paddingRight: "0.5vw",
-            '&-Wrapper': {
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            margin: "auto",
-          },
-          "&-Labels": {
-            display: "flex",
-            justifyContent: "space-between",
-
-            height: "100%",
-            width: "100%",
-            margin: "auto",
-            color: "white",
-            fontWeight: "bold",
-            fontSize: "0.7vw",
-            paddingLeft: "0",
-            backgroundColor: "#001c30 !important",
-          },
-      },
-        },
-      },
-    },
-    MuiButtonBase: {
-      // Styles for the sort label
-      styleOverrides: {
-        root: {
-          height: "100%", // Full height for clickability
-          padding: "0 !important", // No padding for aesthetics
-          "&-MuiIconButton-root": {
-            backgroundColor: "#001c30", // Dark background for contrast
-          },
-        },
-      },
-    },
-    MuiBox: {
-      // Styles for the sort box
-      styleOverrides: {
-        root: {
-          height: "100%", // Full height for clickability
-          padding: "0", // No padding for aesthetics
-          margin: "0", // No margin for aesthetics
-          display: "flex", // Centering for aesthetics
-          alignItems: "center", // Centering for aesthetics
-          justifyContent: "center", // Centering for aesthetics
-        },
-      },
-    },
-    MuiBadge: {
-      // Styles for the sort badge
-      styleOverrides: {
-        root: {
-          height: "100%", // Full height for clickability
-          padding: "5px", // No padding for aesthetics
-          margin: "0", // No margin for aesthetics
-          display: "flex", // Centering for aesthetics
-          alignItems: "center", // Centering for aesthetics
-          justifyContent: "center", // Centering for aesthetics
-          svg: {
-            height: "100%", // Full height for clickability
-            fontSize: "1.1vw", // Responsive font size
-            margin: "0", // No margin for aesthetics
-            padding: "0", // No padding for aesthetics
-            fill: "white ", // White text for contrast
-            strokeWidth: "0.5px", // Thinner stroke for aesthetics
-            opacity: "1", // Slightly transparent for aesthetics
-          },
-        },
-      },
-    },
-
-    MuiSvgIcon: {
-      // Styles for the sort icon
-      styleOverrides: {
-        root: {
-          fontSize: "1.1vw", // Responsive font size
-          fill: "white ", // White text for contrast
-          strokeWidth: "0.5px", // Thinner stroke for aesthetics
-          opacity: "1", // Slightly transparent for aesthetics
-          height: "100%",
-          margin: "0",
-          padding: "0 ",
-        },
-      },
-    },
-    MuiPaginationItem: {
-      styleOverrides: {
-        root: {
-          fontSize: "0.6vw", // Responsive font size
-          fontWeight: "normal", // Bold for emphasis
-          color: "#001c30", // Dark text for contrast
-
-          "&:hover": {
-            color: "#001c30", // Dark text for contrast
-          },
-          "&.Mui-selected": {
-            backgroundColor: "#001c30", // Dark background for contrast
-            color: "white", // White text for contrast
-          },
         },
       },
     },
     MuiTable: {
-      // Styles for the table
+      // Table component.
       styleOverrides: {
         root: {
-          maxWidth: "100%", // Full width
-          height: "100%", // Full height
-          backgroundColor: "transparent", // Clear background for blend
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          maxWidth: "100%",
+          height: "100%",
+        },
+      },
+    },
+    MuiTableHead: {
+      // Table head component.
+      styleOverrides: {
+        root: {
+          position: "sticky",
+          display: "flex",
+          width: "100%",
+          maxWidth: "100%",
+          height: "5%",
+          backgroundColor: "#001C30",
+        },
+      },
+    },
+    MuiTableFooter: {
+      // Table footer component.
+      styleOverrides: {
+        root: {
+          display: 'none',
+        },
+        },
+    },
+    MuiTableBody: {
+      styleOverrides: {
+        root: {
+          height:"95%",
+          
         },
       },
     },
     MuiSelect: {
       styleOverrides: {
         select: {
-          fontSize: "0.6vw", // Responsive font size
-          fontWeight: "normal", // Regular weight for readability
-          color: "#001c30", // Dark text for contrast
-          backgroundColor: "transparent", // Clear background for blend
+          color: "white",
+          fontSize: "0.8vw",
+          fontWeight: "normal",
           "&:focus": {
-            backgroundColor: "transparent", // Clear background for blend
+            backgroundColor: "transparent",
           },
+        },
+        icon: {
+          color: "white",
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: "white",
+          fontSize: "0.8vw",
+          fontWeight: "normal",
+          "&.Mui-focused": {
+            color: "white",
+          },
+        },
+      },
+    },
+    MuiTableRow: {
+      styleOverrides: {
+        root: { 
+          display: "flex",
+          width:'100%',
+        },
+        head: {
+          backgroundColor: "#001C30",
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          fontSize: "0.6vw",
+          flexShrink: 1,
+          flexGrow: 1,
+          textAlign: "center",
+          alignItems: "center",
+          padding: "5px",
+        },
+        head: {
+          color: "#fff",
+          backgroundColor: "#001c30 !important",
+          justifyContent: "space-around",
+          fontSize: "0.7vw",
+          "& .Mui-TableHeadCell-Content": {
+            justifyContent: "space-between",
+            textAlign: "center",
+            fontWeight: "bold",
+          
+            "&-Labels": {
+              position: "relative",
+              left: '1vw',
+              alignItems: "center",
+            },
+            "&-Actions": {
+              position: "relative",
+              fill: "white",
+              right: 0,
+              "& .MuiIconButton-root": {
+                color: "white",
+              },
+            },
+          },
+        },
+      },
+    },
+    MuiTableSortLabel: {
+      styleOverrides: {
+        root: {
+          "& .MuiTableSortLabel-icon": {
+            fill: "white",
+          },
+        },
+      },
+    },
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: "#001C30",
+          color: "white",
+          margin: 0,
+          width: "fit-content",
+          height: "fit-content",
+          position: "absolute",
+          borderRadius: "0",
+        },
+        list: {
+          padding: "0",
+          margin: "0",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        },
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        gutters: {
+          color: "white",
+          justifyContent: "center",
+          textAlign: "center",
+          "&:hover": {
+            backgroundColor: "#001C30",
+          },
+        },
+      },
+    },
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          color: "white",
+        },
+      },
+    },
+    MuiBox: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#001C30",
+        },
+      },
+    },
+    MuiButtonBase: {
+      styleOverrides: {
+        root: {
+          "&.MuiIconButton-root": {
+            color: "white",
+          },
+        },
+      },
+    },
+    MuiTablePagination: {
+      styleOverrides: {
+        root: {
+          width: "100%",
+          justifyContent: "space-between",
+          
+       },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        body2: {
+          color: "white",
         },
       },
     },
@@ -273,14 +315,14 @@ export default function EmployeesTable({ datas }) {
     enableStickyFooter: true,
     enableHiding: false,
     enableColumnFiltersModes: true,
-    enableDensityToggle: true,
+    enableDensityToggle: false,
     enablePagination: true,
     enableToolbar: false,
-    enableFullScreenToggle: false,
+    enableFullScreenToggle: true,
     manualPagination: false,
     paginationDisplayMode: "default",
     muiPaginationProps: {
-      color: "primary",
+      color: "#001c30",
       variant: "outlined",
       shape: "rounded",
       rowsPerPageOptions: [10, 25, 50, 100],
@@ -292,10 +334,8 @@ export default function EmployeesTable({ datas }) {
     debugPagination: true,
     debugFilters: true,
     defaultColumn: {
-      size: 290,
-      height: "100px",
-      align: "center",
-      headerAlign: "center",
+      maxsize: 300,
+      minsize: 40,
     },
   });
 
